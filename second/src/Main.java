8f9f263661f9a04ba5b7d5f76839cb6d6417a74e
wlxorjs2 <wlxorjs2@naver.com>
2023-02-11 00:31:34 +0900

    [Solve] BJ1008

--- second/src/Main.java
+++ second/src/Main.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int a = in.nextInt();
-        int b = in.nextInt();
-        System.out.println(a*b);
+        double a = in.nextDouble();
+        double b = in.nextDouble();
+        System.out.println(a/b);
     }
 }
