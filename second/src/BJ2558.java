aa9c7f9a6fbe7a62544bfbb5462f8a72180cf17c
wlxorjs2 <wlxorjs2@naver.com>
2023-02-12 01:58:11 +0900

    [Solve] BJ2558

--- second/src/BJ2558.java
+++ second/src/BJ2558.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Main {
+public class BJ2558 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int a = in.nextInt();
