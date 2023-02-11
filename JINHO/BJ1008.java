c64080ea424e18f7077eb26d173eb1054df7922f
wlxorjs2 <wlxorjs2@naver.com>
2023-02-12 01:53:47 +0900

    [Solve] BJ10430

---
+++ JINHO/BJ1008.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class BJ1008 {
+        public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+            double a = in.nextDouble();
+            double b = in.nextDouble();
+            System.out.println(a/b);
+        }
+    }
+
--- JINHO/BJ11718.java
+++ JINHO/BJ11718.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import java.util.Scanner;
 public class BJ11718 {
     public static void main(String[] args) {
@@ -8,3 +9,15 @@ public class BJ11718 {
         }
     }
 }
+=======
+import java.util.Scanner;
+public class BJ11718 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while(in.hasNextLine()) {
+            String a = in.nextLine();
+            System.out.println(a);
+        }
+    }
+}
+>>>>>>> 10f25e195b4c753569f7f2898e7f4c6d644af486
